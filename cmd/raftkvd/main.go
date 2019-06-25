f6c7c5186178c31f6305b23b65ecb9068b2c0a35
ejunjsh <sjj050121014@163.com>
2019-06-25 18:16:33 +0800

    update main code

--- cmd/raftkvd/main.go
+++ cmd/raftkvd/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"github.com/ejunjsh/raftkv/pkg/raft/raftpb"
+	"strings"
+)
 
+func main() {
+	cluster := flag.String("cluster", "http://127.0.0.1:9021", "comma separated cluster peers")
+	id := flag.Int("id", 1, "node ID")
+	kvport := flag.Int("port", 9121, "key-value server port")
+	join := flag.Bool("join", false, "join an existing cluster")
+	flag.Parse()
+
+	proposeC := make(chan string)
+	defer close(proposeC)
+	confChangeC := make(chan raftpb.ConfChange)
+	defer close(confChangeC)
+
+	// raft provides a commit stream for the proposals from the http api
+	var kvs *kvstore
+	getSnapshot := func() ([]byte, error) { return kvs.getSnapshot() }
+	commitC, errorC, snapshotterReady := newRaftNode(*id, strings.Split(*cluster, ","), *join, getSnapshot, proposeC, confChangeC)
+
+	kvs = newKVStore(<-snapshotterReady, proposeC, commitC, errorC)
+
+	// the key-value http handler will propose updates to raft
+	serveHttpKVAPI(kvs, *kvport, confChangeC, errorC)
 }
